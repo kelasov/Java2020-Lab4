de642b7d060008bb9b1726a1518b516d4eb8541d
kelasov <kelasovg@mail.ru>
2020-09-21 10:31:55 +0300

    init

---
+++ src/Lab4/Circle.java
@@ -0,0 +1,42 @@
+package Lab4;
+
+public class Circle extends Shape {
+    protected double radius;
+    public Circle(){}
+
+    public Circle(double radius){
+        this.radius=radius;
+    }
+    public Circle(double radius, String Color, boolean filled){
+        this.radius=radius;
+        this.color=Color;
+        this.filled=filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI*radius*radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*Math.PI*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius='" + radius + '\'' +
+                "color='" + color + '\'' +
+                "filled=" + filled +
+                '}';
+    }
+}
